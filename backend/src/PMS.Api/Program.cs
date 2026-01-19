5cc154a45ff07c069376c046edeec88a626a5a22
ZeroMercy-source <144501827+ZeroMercy-source@users.noreply.github.com>
2026-01-19 18:46:59 -0500

    adding stages to yaml with new environment to test healthcheck before prod

--- backend/src/PMS.Api/Program.cs
+++ backend/src/PMS.Api/Program.cs
@@ -1,13 +1,17 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using PMS.Api.Data;
 using PMS.Api.Services;
-using System;
+using HealthChecks.UI.Client;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<PmsDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
+builder.Services.AddDbContext<PmsDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddHealthChecks().AddSqlServer(connectionString);
 
 var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>() 
     ?? new[] { "http://localhost:3000" };
@@ -29,7 +33,6 @@ builder.Services.AddScoped<ProjectServices>();
 builder.Services.AddScoped<TaskServices>();
 builder.Services.AddScoped<SubTaskServices>();
 
-builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -43,5 +46,10 @@ app.UseCors("Frontend");
 
 app.UseAuthorization();
 app.MapControllers();
-app.UseHealthChecks("/health");
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 app.Run();
